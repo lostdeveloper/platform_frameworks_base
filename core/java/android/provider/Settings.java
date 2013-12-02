0428f2916f809b222cfdff267992635fb384470e
travp624 <travp624@gmail.com>
2013-12-02 00:03:17 -0600

    Frameworks: Lockscreen custom wallpaper (1/2)
    kitkat now has code for a custom wallpaper image.
    
    Using that code set a switch case to set the custom background between
    	- Color Fill
    	- Custom Wallpaper
    	- Default background
    
    This way allows the same overlay shadow thats set on the keyguard when
    its the defualt way. And the overlay effect works on both color fill and
    custom image.
    
    PatchSet 3: Missed the simple fact this also sets the album art when
                transport view is active. This fixes that.
    
    PatchSet 4: edit commit message.
    
    PatchSet 5: rebase to work with commit b3c0350c9f48dcdaa46326792b21964a246f76d5
    
    PatchSet 6: fix commit message.
    
    PatchSet 7: fix background not setting after a screen on screen off then
                screen on.
    
    Change-Id: I98fde63ef9f3c0a5f334cd9a97b237a6f99c57f6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2970,13 +2970,13 @@ public final class Settings {
         public static final String STATUS_BAR_NETWORK_ACTIVITY = "status_bar_network_activity";
 
         /**
-          * Volume keys control cursor in text fields (default is 0)
-          * 0 - Disabled
-          * 1 - Volume up/down moves cursor left/right
-          * 2 - Volume up/down moves cursor right/left
-          * @hide
-          */
-         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+         * Volume keys control cursor in text fields (default is 0)
+         * 0 - Disabled
+         * 1 - Volume up/down moves cursor left/right
+         * 2 - Volume up/down moves cursor right/left
+         * @hide
+         */
+        public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
          * show clear all recents button
@@ -3008,6 +3008,25 @@ public final class Settings {
          * @hide
          */
         public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
+        /**
+         * Sets the lockscreen background style
+         * @hide
+         */
+        public static final String LOCKSCREEN_BACKGROUND_STYLE = "lockscreen_background_style";
+
+        /**
+         * Color for lockscreen background when set to color fill
+         * @hide
+         */
+        public static final String LOCKSCREEN_BACKGROUND_COLOR = "lockscreen_background_color";
+
+        /**
+         * Alpha value for custom image on lockscreen background
+         * when set to custom image
+         * @hide
+         */
+        public static final String LOCKSCREEN_WALLPAPER_ALPHA = "lockscreen_wallpaper_alpha";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -7084,4 +7103,3 @@ public final class Settings {
         return "android-" + Long.toHexString(androidId);
     }
 }
-
