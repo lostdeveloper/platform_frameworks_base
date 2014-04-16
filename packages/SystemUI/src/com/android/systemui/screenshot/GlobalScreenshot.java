77d95385ae4717132d6500d18a0c71b54d55915b
AndroidRul3z <ferrettidario@gmail.com>
2014-04-16 16:40:41 +0200

    Revert "Avoid possible memory leaks by ensuring stream closures"
    This reverts commit be9f12f20cbeb393bfb1f3746ceb3aa13384fcc7.

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -60,7 +60,6 @@ import com.android.systemui.R;
 import com.android.systemui.screenshot.DeleteScreenshot;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -186,14 +185,14 @@ class SaveImageInBackgroundTask extends AsyncTask<SaveImageInBackgroundData, Voi
             return null;
         }
 
-        // By default, AsyncTask sets the worker thread to have background thread priority, so
-        // give highest possible priority to worker thread
-        Process.setThreadPriority(Process.THREAD_PRIORITY_URGENT_DISPLAY);
+        // By default, AsyncTask sets the worker thread to have background thread priority, so bump
+        // it back up so that we save a little quicker.
+        Process.setThreadPriority(Process.THREAD_PRIORITY_FOREGROUND);
 
         Context context = params[0].context;
         Bitmap image = params[0].image;
         Resources r = context.getResources();
-        OutputStream outStream = null;
+
         try {
             // Create screenshot directory if it doesn't exist
             mScreenshotDir.mkdirs();
@@ -243,8 +242,10 @@ class SaveImageInBackgroundTask extends AsyncTask<SaveImageInBackgroundData, Voi
                      PendingIntent.getBroadcast(context, 0, deleteIntent,
                      PendingIntent.FLAG_CANCEL_CURRENT));
 
-            outStream = resolver.openOutputStream(uri);
-            image.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            OutputStream out = resolver.openOutputStream(uri);
+            image.compress(Bitmap.CompressFormat.PNG, 100, out);
+            out.flush();
+            out.close();
 
             // update file size in the database
             values.clear();
@@ -259,16 +260,13 @@ class SaveImageInBackgroundTask extends AsyncTask<SaveImageInBackgroundData, Voi
             // mounted
             params[0].clearImage();
             params[0].result = 1;
-        } finally {
-            if (outStream != null) {
-                try {
-                    outStream.flush();
-                    outStream.close();
-                } catch (IOException ioe) {
-                    // let it go
-                }
-            }
         }
+
+        // Recycle the bitmap data
+        if (image != null) {
+            image.recycle();
+        }
+
         return params[0];
     }
 
